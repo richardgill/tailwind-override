51383848451b2345547cbfc08160b626ffbedc84
Richard Gill <richard@rgill.co.uk>
2021-08-08 09:12:44 +0100

    made it possible to not import large json file

---
+++ packages/tailwind-override/test/defaultTests.ts
@@ -0,0 +1,15 @@
+export const defaultTestCases = [
+  { input: '', expectedOutput: '' },
+  { input: 'apple oranges', expectedOutput: 'apple oranges' },
+  { input: 'apple   oranges', expectedOutput: 'apple oranges' },
+  { input: 'bg-yellow-600 bg-yellow-700', expectedOutput: 'bg-yellow-700' },
+  { input: 'bg-yellow-600 bg-yellow-700 bg-yellow-800', expectedOutput: 'bg-yellow-800' },
+  { input: 'bg-yellow-600   bg-yellow-700', expectedOutput: 'bg-yellow-700' },
+  { input: 'w-1/2 w-1/3', expectedOutput: 'w-1/3' },
+  { input: 'text-pink-200 text-blue-200', expectedOutput: 'text-blue-200' },
+  { input: 'pt-2 pt-4', expectedOutput: 'pt-4' },
+  { input: 'pt-2 apple pt-4 orange', expectedOutput: 'apple pt-4 orange' },
+  { input: 'pt-2 apple pt-4 orange text-pink-200', expectedOutput: 'apple pt-4 orange text-pink-200' },
+  { input: 'pt-4 text-pink-200', expectedOutput: 'pt-4 text-pink-200' },
+  { input: 'prefix-pt-2 apple prefix-pt-4 orange', options: { prefix: 'prefix-' }, expectedOutput: 'apple prefix-pt-4 orange' },
+]
