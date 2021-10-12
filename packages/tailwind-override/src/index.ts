40c2e75f576e49017377fb32a17bfcf58de93fbe
Richard Gill <richard@rgill.co.uk>
2021-10-12 07:19:48 +0100

    export json

--- packages/tailwind-override/src/index.ts
+++ packages/tailwind-override/src/index.ts
@@ -1,12 +1,14 @@
-import tailwindProperties from './tailwindcssProperties.json'
+import tailwindPropertiesJson from './tailwindcssProperties.json'
 import { overrideTailwindClasses as overrideTailwindClassesCore } from './core'
 import type { OptionsArg } from './core'
 
 const defaultOptions = {
   prefix: '',
-  tailwindProperties,
+  tailwindProperties: tailwindPropertiesJson,
 }
 
 export const overrideTailwindClasses = (classNamesString: string, optionsArg?: OptionsArg) => {
   return overrideTailwindClassesCore(classNamesString, { ...defaultOptions, ...optionsArg })
 }
+
+export const tailwindProperties = tailwindPropertiesJson
