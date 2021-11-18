b9748a1648d1aa5fffc5b0dfdb36fd31ce2c501b
Richard Gill <richard@rgill.co.uk>
2021-11-18 08:53:07 +0000

    removed console.logs

--- test/performance.test.ts
+++ test/performance.test.ts
@@ -8,25 +8,21 @@ import { Options, overrideTailwindClasses } from '../src/index'
 import { allTestCases } from './testCases'
 
 test(`overrideTailwindClasses performance`, () => {
-  console.log('number of tests: ', allTestCases.length)
   const start = performance.now()
   for (const testCase of allTestCases) {
     overrideTailwindClasses(testCase.input, { ...testCase.options, ruleLookupCache: false } as Options)
   }
   const end = performance.now()
-  console.log(start, end, end - start)
   expect(end - start).toBeLessThan(300)
 })
 
 const duplicatedTestCases = _.times(1000).flatMap(() => allTestCases)
 
 test(`overrideTailwindClasses performance`, () => {
-  console.log('number of tests: ', duplicatedTestCases.length)
   const start = performance.now()
   for (const testCase of duplicatedTestCases) {
     overrideTailwindClasses(testCase.input, { ...testCase.options, ruleLookupCache: false } as Options)
   }
   const end = performance.now()
-  console.log(start, end, end - start)
   expect(end - start).toBeLessThan(1000)
 })
